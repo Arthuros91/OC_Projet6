77f031353729c6493cf26e436b1eaeb925912094
Arthuros91 <arthur-a54@hotmail.fr>
2023-06-21 18:29:24 +0200

    header and footer responsive done

---
+++ kasa_2/src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import ErrorPage from './pages/404';
+import Location from './pages/location';
+import About from './pages/about';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Location" element={<Location />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
+
